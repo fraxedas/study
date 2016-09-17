d89839bd395d27533a2328111879d2b7edc5cdf8
Oscar Fraxedas <fraxedas@gmail.com>
2016-09-17 13:50:40 -0400

    Find the minimum element in rotated array

--- test/array.js
+++ test/array.js
@@ -5,7 +5,7 @@ var array = require("../lib/array");
 describe('array', function () {
 
     describe('of [3,5,6,7,8,9]', function () {
-        var nums = [3,5,6,7,8,9];
+        var nums = [3, 5, 6, 7, 8, 9];
 
         it('should return true when seaching for 5', function () {
 
@@ -16,6 +16,38 @@ describe('array', function () {
         it('should return false when seaching for 4', function () {
 
             assert.equal(array.binary_search(nums, 4), false);
+
+        });
+    });
+
+    describe('rotated_search_min', function () {
+        it('should return 0 for [3, 5, 6, 7, 8, 9, 0, 1, 2]', function () {
+
+            assert.equal(array.rotated_search_min([3, 5, 6, 7, 8, 9, 0, 1, 2]), 0);
+
+        });
+
+        it('should return 1 for [1]', function () {
+
+            assert.equal(array.rotated_search_min([1]), 1);
+
+        });
+
+        it('should return 1 for [1,2]', function () {
+
+            assert.equal(array.rotated_search_min([1,2]), 1);
+
+        });
+
+        it('should return 1 for [2,1]', function () {
+
+            assert.equal(array.rotated_search_min([2,1]), 1);
+
+        });
+
+        it('should return 1 for [2,1]', function () {
+
+            assert.equal(array.rotated_search_min([4,5,6,1,2,3]), 1);
 
         });
     });
