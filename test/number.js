7d404dbae4da8753414f4f04cef5835028b38593
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2016-09-20 16:53:34 -0400

    isUgly

---
+++ test/number.js
@@ -0,0 +1,34 @@
+
+var assert = require("assert");
+var number = require("../lib/number");
+
+describe('number', function () {
+
+    describe('isUgly', function () {
+
+        it('should return true for 1', function () {
+
+            assert.equal(number.isUgly(1), true);
+
+        });
+
+        it('should return true for 6', function () {
+
+            assert.equal(number.isUgly(6), true);
+
+        });
+
+        it('should return false for 14', function () {
+
+            assert.equal(number.isUgly(14), false);
+
+        });
+
+        it('should return false for 0', function () {
+
+            assert.equal(number.isUgly(0), false);
+
+        });
+    });
+
+});
